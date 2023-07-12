16c12b859c3fd8c8f106abb51c017298eec16bd1
Sanchita Das Gupta <sanchita.gupta@intelivideo.com>
2023-07-12 17:11:11 +0530

    package added

---
+++ Sources/AdDisplaySDK/AdDisplaySDK.swift
@@ -0,0 +1,10 @@
+public struct AdDisplaySDK {
+    public private(set) var text = "Hello, World!"
+
+    public static func getName() -> String {
+        return "sonaiiii"
+    }
+    
+    public init() {
+    }
+}
---
+++ Tests/AdDisplaySDKTests/AdDisplaySDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AdDisplaySDK
+
+final class AdDisplaySDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AdDisplaySDK().text, "Hello, World!")
+    }
+}
